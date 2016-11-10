83eedb640547d516a53dbe15455cdbccc46ff10b
Nir Moav <nirm@wix.com>
2016-11-10 21:05:12 +0200

    creating a node server and endpoint

--- src/server.js
+++ src/server.js
@@ -3,11 +3,48 @@
 const http = require('http');
 const https = require('https');
 
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-function start() {
-  console.log('Starting Bitly Resolver...');
+const DEFAULT_PORT = 5000;
+
+class BitlyResolver {
+  constructor() {
+    this.startServer();
+  }
+
+  startServer() {
+    const app = express();
+    this.configAppServerMiddleware(app);
+    this.configureServerRouting(app);
+
+    app.listen(app.get('port'), () => {
+      console.log('BitlyResolver Server is running on port', app.get('port'));
+    });
+  }
+
+  configAppServerMiddleware(app) {
+    app.use(cors());
+    app.use(bodyParser.json()); // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodies
+
+    app.set('port', (process.env.PORT || DEFAULT_PORT));
+  }
+
+  configureServerRouting(app) {
+    app.get('/resolveBitly', this.resolveBitly);
+  }
+
+  resolveBitly(req, res) {
+    const urlToResolve = req.query.url;
+    let resolvedURL = 'http://no-idea-yet/' + urlToResolve;
+
+    res.json({
+        status: '200',
+        resolvedUrl: resolvedURL
+      });
+  }
 }
 
-
-start();
+new BitlyResolver();
