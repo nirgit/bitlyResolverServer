3712afbd5dd5deda4465544fb288b9c843081e0d
Nir Moav <nirm@wix.com>
2016-11-10 20:45:38 +0200

    initial commit.

---
+++ src/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const http = require('http');
+const https = require('https');
+
+// const express = require('express');
+
+function start() {
+  console.log('Starting Bitly Resolver...');
+}
+
+
+start();
